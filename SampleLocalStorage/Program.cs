6334a9b56b7e3cdee038cdaaad4190c580a8d8da
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2023-06-06 22:02:28 +0200

    lab: simple app to read text file from local emulator without auth

---
+++ SampleLocalStorage/Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Storage.Blobs;
+
+string connectionString = "UseDevelopmentStorage=true";
+string containerName = "sample-container";
+string blobName = "sample-blob.txt";
+        
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+var response = await blobClient.DownloadAsync();
+using var streamReader = new StreamReader(response.Value.Content);
+string content = await streamReader.ReadToEndAsync();
+Console.WriteLine(content);
